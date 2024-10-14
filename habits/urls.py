0f862c2a7a5a9f2379abe47c33cea424430cec5e
valya_plotnikova <valya_ezhova@mail.ru>
2024-10-14 15:14:41 +0300

    create urls users and habits

---
+++ habits/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from habits.apps import HabitsConfig
+from habits.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, \
+    HabitDestroyAPIView, HabitPublishedListAPIView
+
+app_name = HabitsConfig.name
+
+urlpatterns = [
+    path('habit/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habit/', HabitListAPIView.as_view(), name='habit-list'),
+    path('habit/published/', HabitPublishedListAPIView.as_view(), name='habit-published'),
+    path('habit/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit-get'),
+    path('habit/update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit-update'),
+    path('habit/delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='habit-delete'),
+]
